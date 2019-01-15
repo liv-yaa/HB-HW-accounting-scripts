2e25d4b4aa17b17c32cfae77021e33d84d815ee2
liv-yaa <olivia.f.smith@gmail.com>
2019-01-14 20:18:21 -0800

    Added text

--- melon_info.py
+++ melon_info.py
@@ -1,4 +1,8 @@
-"""Print out all the melons in our inventory."""
+"""Print out all the melons in our inventory.
+Edit both files to track flesh color, rind color, and average weight of melons. 
+lOutput should look something like:
+
+"""
 
 
 from melons import melon_names, melon_seedlessness, melon_prices
@@ -11,7 +15,17 @@ def print_melon(name, seedless, price):
     if seedless:
         have_or_have_not = 'do not have'
 
-    print(f"{name}s {have_or_have_not} seeds and are ${price:.2f}")
+    # print(f"{name}s {have_or_have_not} seeds and are ${price:.2f}")
+
+    print(f"""
+        {name.upper()}
+          seedless: {seedless}
+          price: {price}
+          flesh_color: None
+          weight: None
+          rind_color: None
+          """)
+
 
 
 for i in melon_names:
